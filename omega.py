5d42de4272e17a81e49b51608bf0397dfdbb3edb
Tristan Edwards <52096006+kanrinsha@users.noreply.github.com>
2019-06-23 22:10:30 -0700

    Update omega.py

--- omega.py
+++ omega.py
@@ -6,8 +6,11 @@ import speech_recognition
 import configparser
 import time
 import wolframalpha
+import urllib
+import re
 from commands.tts import text_to_speech
 from commands.smart_home import device_control
+from commands.smart_home import chromecast
 from commands.common import recognize_lists, weather
 
 
@@ -35,17 +38,69 @@ def recognize_callback():
         text_to_speech.mixer.music.load('audio/on.wav')
         text_to_speech.mixer.music.play()
         text_to_speech.speech_control('pause')
-        audio = recognizer.listen(mic_source, timeout=7.5, phrase_time_limit=15.5)
+        audio = recognizer.listen(mic_source, timeout=6, phrase_time_limit=15.5)
+        text_to_speech.mixer.init()
+        text_to_speech.mixer.music.load('audio/recog.wav')
+        text_to_speech.mixer.music.play()
     try:
         recognizer_result = recognizer.recognize_google(audio).lower()
         print(recognizer_result)
 # smart home
         if 'turn off the ' in recognizer_result:
-            device_off = recognizer_result.replace('turn off the ', '').replace(' ', '_')
-            device_control.toggle(device_off, 'off')
+            device_off = str(recognizer_result).replace('turn off the ', '').split()
+            if 'on' in recognizer_result:
+                for item, elem in enumerate(recognize_lists.bad_device_list):
+                    while elem in device_off:
+                        device_off.remove(elem)
+                        print(device_off)
+                if recognizer_result.find('on') < recognizer_result.find(device_off[0]):
+                    del device_off[0]
+                    print(device_off)
+                if recognizer_result.find('on') > recognizer_result.find(device_off[0]):
+                    del device_off[1]
+                    print(device_off)
+                for device in device_off:
+                    device_control.toggle(device, 'off')
+            else:
+                for device in device_off:
+                    device_control.toggle(device, 'off')
+
         if 'turn on the ' in recognizer_result:
-            device_on = recognizer_result.replace('turn on the ', '').replace(' ', '_')
-            device_control.toggle(device_on, 'on')
+            device_on = str(recognizer_result).replace('turn on the ', '').split()
+            if 'off' in recognizer_result:
+                for item, elem in enumerate(recognize_lists.bad_device_list):
+                    while elem in device_on:
+                        device_on.remove(elem)
+                        print(device_on)
+                if recognizer_result.find('off') < recognizer_result.find(device_on[0]):
+                    del device_on[0]
+                    print(device_on)
+                if recognizer_result.find('off') > recognizer_result.find(device_on[0]):
+                    del device_on[1]
+                    print(device_on)
+                for device in device_on:
+                    device_control.toggle(device, 'on')
+            else:
+                for device in device_on:
+                    device_control.toggle(device, 'on')
+
+        if 'chromecast stream ' in recognizer_result:
+            cut = recognizer_result.replace('chromecast stream ', '')
+            if 'movie' in cut:
+                movie = cut.replace('movie', '')
+                print(movie)
+                text_to_speech.tts("Searching and streaming movie " + movie)
+                chromecast.chromecast_control('movie', movie)
+            if 'youtube' in cut:
+                string = cut.replace("youtube ", "")
+                query_string = urllib.parse.urlencode({"search_query": string})
+                html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+                search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+                url = "http://www.youtube.com/watch?v=" + search_results[0]
+                print(url)
+                chromecast.chromecast_control('youtube', url)
+                text_to_speech.tts("streaming " + string + " from youtube")
+
 # common
         if any(time_result in recognizer_result for time_result in recognize_lists.time_list):
             text_to_speech.tts(time.strftime("%I:%M %p"))
@@ -78,6 +133,8 @@ def recognize_callback():
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
     except AttributeError:
         print('ugh oh...')
+    except configparser.NoOptionError:
+        print('no device...')
 
 
 model = 'Omega.pmdl'
@@ -86,5 +143,4 @@ print('Waiting for keyword....')
 
 detector.start(detected_callback=recognize_callback,
                sleep_time=0.03)
-
 detector.terminate()
