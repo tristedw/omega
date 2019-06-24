be95ee66008ad5f885e42c243640201b52c18a5e
Tristan Edwards <52096006+kanrinsha@users.noreply.github.com>
2019-06-23 22:08:28 -0700

    Update recognize_lists.py

--- commands/common/recognize_lists.py
+++ commands/common/recognize_lists.py
@@ -3,3 +3,4 @@ date_list = ["what's the date", "what day is it", "which day is it", "what date 
 weather_list = ["what's the weather", "what is the weather", "what's the weather outside",
                 "what is the weather outside", "what's the temperature", "what is the temperature",
                 "how hot is it", "how cold is it"]
+bad_device_list = ['turn', 'off', 'on', 'the']
